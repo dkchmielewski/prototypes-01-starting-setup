158f3f6a6a72e526d073dafd22c195f93c60a466
dkchmielewski <66046504+dkchmielewski@users.noreply.github.com>
2023-02-02 15:26:33 -0500

    setting and getting prototypes

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-// class AgedPerson {
-//     printAge() {
-//         console.log(this.age);
-//     }
-// }
+class AgedPerson {
+  printAge() {
+    console.log(this.age);
+  }
+}
 
-// class Person extends AgedPerson {
-//     name = 'Max';
+class Person extends AgedPerson {
+  name = 'Max';
 
-//     constructor() {
-//         super();
-//         this.age = 30;
-//     }
+  constructor() {
+    super();
+    this.age = 30;
+  }
 
-//     greet() {
+  greet() {
+    console.log(
+      'Hello my name is ' + this.name + ' and I am ' + this.age + ' years old.'
+    );
+  }
+}
+
+// const person = new Person();
+// person.greet();
+
+// function Person() {
+//     this.age = 30;
+//     this.name = 'Max';
+//     this.greet = function() {
 //         console.log('Hello my name is ' + this.name + ' and I am ' + this.age + ' years old.');
 //     }
 // }
@@ -20,27 +33,58 @@
 // const person = new Person();
 // person.greet();
 
-function Person() {
-    this.age = 30;
-    this.name = 'Max';
-    this.greet = function() {
-        console.log('Hello my name is ' + this.name + ' and I am ' + this.age + ' years old.');
-    }
-}
+// Person.prototype.printAge = function() {
+//         console.log(this.age);
+// };
 
-const person = new Person();
-person.greet();
+// console.dir(Person);
+// const p = new Person();
+// p.greet();
+// p.printAge();
+// console.log(p.length);
+// console.log(p.toString());
+// console.log(p.__proto__);
+// const p2 = new p.__proto__.constructor();
+// console.dir(Object.prototype.__proto__);
 
-Person.prototype.printAge = function() {
-        console.log(this.age);
+const course = {
+  //new Object()
+  title: 'JS Guide',
+  rating: 5,
 };
+console.log(Object.getPrototypeOf(course));
 
-console.dir(Person);
-const p = new Person();
-p.greet();
-p.printAge();
-console.log(p.length);
-console.log(p.toString());
-console.log(p.__proto__);
-const p2 = new p.__proto__.constructor();
-console.dir(Object.prototype.__proto__);
+Object.setPrototypeOf(course, {
+  // ...Object.getPrototypeOf(course),
+  printRating: function () {
+    console.log(`${this.rating}/5`);
+  },
+});
+
+const student = Object.create({
+  printProgress: function () {
+    console.log(this.progress);
+  },
+}, {
+    name: {
+        configurable: true,
+        enumerable: true,
+        value: 'Max',
+        writable: true
+    }
+});
+
+// student.name = 'Max';
+
+Object.defineProperty(student, 'progress', {
+    configurable: true,
+    enumerable: true,
+    value: 0.8,
+    writable: false
+});
+
+student.printProgress();
+
+console.log(student);
+
+course.printRating();
